eff49f5f82cb4c599e3aa96ba38d595713fc0357
alain <79479952+chadnpc@users.noreply.github.com>
2025-04-01 17:54:24 +0200

    Update Test-Module.ps1

--- Test-Module.ps1
+++ Test-Module.ps1
@@ -1,10 +1,10 @@
 ﻿
 #!/usr/bin/env pwsh
 # .SYNOPSIS
-#   MCP testScript v0.1.0
+#   MCP testScript v0.1.1
 # .EXAMPLE
-#   ./Test-Module.ps1 -version 0.1.0
-#   Will test the module in ./BuildOutput/MCP/0.1.0/
+#   ./Test-Module.ps1 -version 0.1.1
+#   Will test the module in ./BuildOutput/MCP/0.1.1/
 # .EXAMPLE
 #   ./Test-Module.ps1
 #   Will test the latest  module version in ./BuildOutput/MCP/
